8c35b3a08b21bd1f8667714d9e6d8ab64a7fdbed
Oz Katz <oz.katz@treeverse.io>
2024-03-09 22:50:49 +0200

    yolo!

--- taxi-pipelines/taxi_pipelines/__init__.py
+++ taxi-pipelines/taxi_pipelines/__init__.py
@@ -1,11 +1,8 @@
 from dataclasses import dataclass
 from dagster import Definitions, job, op, Config, OpExecutionContext
 
-import lakefs
 import pandas as pd
 
-DAGSTER_RUNS_URL = 'http://127.0.0.1:3000/runs'
-EXTERNAL_INPUT_SOURCE = 'nyc_taxi.parquet'
 
 class RawConfig(Config):
     raw_path_uri: str
@@ -13,30 +10,10 @@ class RawConfig(Config):
 class PathConfig(Config):
     base_path: str
 
-class BranchConfig(Config):
-    repository: str
-    branch: str
-
-
-@dataclass
-class Sandbox:
-    repository: str
-    branch: str
 
 @op
-def create_sandbox(config: BranchConfig) -> Sandbox:
-    lakefs.repository(config.repository).\
-        branch(config.branch).\
-        create(source_reference='main', exist_ok=True)
-    return Sandbox(
-        repository=config.repository, 
-        branch=config.branch, 
-    )
-
-
-@op
-def sandbox_uri(config: PathConfig, sandbox: Sandbox) -> str:
-    return f'lakefs://{sandbox.repository}/{sandbox.branch}/{config.base_path}'
+def get_yolo_uri(config: PathConfig) -> str:
+    return config.base_path
 
 
 @op
@@ -66,27 +43,12 @@ def partition_nyc_taxi(base_uri: str, clean_taxi_data: str) -> str:
     df.to_parquet(output_path, partition_cols=['year', 'month', 'day'], existing_data_behavior='delete_matching')
     return output_path
 
-
-@op
-def commit_changes(context: OpExecutionContext, sandbox: Sandbox, partitioned_location: str):
-    branch = lakefs.repository(sandbox.repository).branch(sandbox.branch)
-    branch.commit(
-        'NYC Taxi Dagster run completed',
-        metadata={
-            'dagster_run_id': context.run.run_id,
-            '::lakefs::Dagster::url[url:ui]': f'{DAGSTER_RUNS_URL}/{context.run.run_id}'
-        },
-    )
-
-
 @job
 def nyc_taxi_job():
-    sandbox = create_sandbox()
-    base_uri = sandbox_uri(sandbox)
+    base_uri = get_yolo_uri()
     ingested_loc = raw_taxi_data(base_uri)
     clean_loc = clean_nyc_taxi(base_uri, ingested_loc)
     partitioned_location = partition_nyc_taxi(base_uri, clean_loc)
-    commit_changes(sandbox, partitioned_location)
 
 
 defs = Definitions(jobs=[nyc_taxi_job])
